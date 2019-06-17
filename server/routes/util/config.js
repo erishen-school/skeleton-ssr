b101948f847f6e432b7e498829517e66ee3bbb95
Erishen <lei_sun@Ctrip.com>
2019-06-17 10:16:45 +0800

    update

--- server/routes/util/config.js
+++ server/routes/util/config.js
@@ -4,5 +4,8 @@
 export default {
     github: [
         { action: 'zeit', links: ['swiper.min.css'], scripts: ['swiper.min.js'] }
+    ],
+    math: [
+        { action: 'quiz', scripts: ['jquery-3.3.1.min.js'] }
     ]
 };
